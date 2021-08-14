f918692defa2e03cb08634fe298f21cd933ff580
trash0000 <trash0000@mail.ru>
2021-08-14 16:22:59 +0300

    lesson 5 is complete

--- src/main/java/Lesson5/employee.java
+++ src/main/java/Lesson5/employee.java
@@ -7,6 +7,18 @@ public class employee {
     public String mail;
     public int age;
     public int cell;
+public employee(String name, String position, String mail, int cell, int salary,int age) {
+    this.name = name;
+    this.position = position;
+    this.mail = mail;
+    this.cell = cell;
+    this.salary = salary;
+    this.age = age;
+}
+public void info(){
+    System.out.println(this.name + ", " + this.position + ", " + this.mail + ", " + this.cell + ", " + this.salary + ", " + this.age);
+}
+
 
 
 }
--- src/main/java/Lesson5/employeeApp.java
+++ src/main/java/Lesson5/employeeApp.java
@@ -2,15 +2,20 @@ package Lesson5;
 
 public class employeeApp {
     public static void main(String[] args) {
-        employee employee1 = new employee();
-        System.out.println(employee1.name + ", " + employee1.position + ", " + employee1.mail + ", " + employee1.cell + ", "+  employee1.age);
-employee1.name = "Иванов Иван Иванович";
-employee1.position = "Директор";
-employee1.mail = "iv@mail.ru";
-employee1.cell = 678;
-employee1.age = 45;
+//        employee employee1 = new employee("Иванов Иван Иванович", "Директор", "iv@mail.ru", 678, 45, 250000);
+//        employee1.info();
+        employee[] empArr = new employee[5];
+        empArr[0] = new employee("Иванов Иван Иванович", "Директор", "iv@mail.ru", 678, 250000, 45);
+        empArr[1] = new employee("Иванов Петр Иванович", "зам.Директор", "pe@mail.ru", 679, 245000, 43);
+        empArr[2] = new employee("Иванов Сергей Иванович", "зам.зам.Директор", "se@mail.ru", 671, 240000, 41);
+        empArr[3] = new employee("Иванов Николай Иванович", "зам.зам.зам.Директор", "ni@mail.ru", 672, 235000, 40);
+        empArr[4] = new employee("Иванов Евгений Иванович", "зам.зам.зам.зам.Директор", "ev@mail.ru", 673, 230000, 39);
 
-
+        for (int i = 0; i < empArr.length; i++){
+            if (empArr[i].age > 40) {
+                System.out.println(empArr[i].name + ", " + empArr[i].position + ", " + empArr[i].mail + ", " + empArr[i].cell + ", " + empArr[i].salary + ", " + empArr[i].age);
+            }
+        }
 
     }
 }
