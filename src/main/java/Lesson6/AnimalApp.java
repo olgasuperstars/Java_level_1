cbabcc8a9bc887ef12807825bbc55a7eb5c84aca
trash0000 <trash0000@mail.ru>
2021-08-18 19:48:37 +0300

    6 is not completed

---
+++ src/main/java/Lesson6/AnimalApp.java
@@ -0,0 +1,7 @@
+package Lesson6;
+
+public class AnimalApp {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/Lesson6/Animals.java
@@ -0,0 +1,11 @@
+package Lesson6;
+
+public abstract class  Animals {
+    public void run() {
+        System.out.println("Animal is running");
+    }
+    public void swim() {
+        System.out.println("Animal is swimming");
+    }
+
+}
