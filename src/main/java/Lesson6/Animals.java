6fd0549a5eae5f840861314463d85df0ede09cf8
trash0000 <trash0000@mail.ru>
2021-08-18 23:10:27 +0300

    6 is completed

--- src/main/java/Lesson6/Animals.java
+++ src/main/java/Lesson6/Animals.java
@@ -1,11 +1,34 @@
 package Lesson6;
 
 public abstract class  Animals {
+    protected String name;
+public static int count = 0;
+    public Animals(String name) {
+        this.name = name;
+        count++;
+    }
+
+    public Animals() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public void run() {
         System.out.println("Animal is running");
     }
     public void swim() {
         System.out.println("Animal is swimming");
     }
+public abstract int runLength();
 
+    public abstract void runLength(int length);
+
+    public abstract void swimLength(int length);
 }
--- src/main/java/Lesson6/Cat.java
+++ src/main/java/Lesson6/Cat.java
@@ -1,5 +1,42 @@
 package Lesson6;
 
 public class Cat extends Animals {
+    public static int count = 0;
+      public Cat(String name) {
+        super(name);
+        count++;
+    }
+
+    public Cat() {
+
+    }
+
+
+    @Override
+    public int runLength() {
+        return 0;
+    }
+
+    @Override
+    public void runLength(int length) {
+
+        if(length > 200)
+
+    {
+        System.out.println(name + " cant run");
+    }
+        else
+
+    {
+        System.out.println(name + " runs " + length);
+
+    }
+}
+
+    @Override
+    public void swimLength(int length) {
+        System.out.println(name + " cant swim");
+    }
+
 
 }
